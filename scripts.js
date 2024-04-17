f272be53c00151fa6fbb03639e008448eb3d2364
Leonardo Ibiapina <161875822+ibiapleo@users.noreply.github.com>
2024-04-17 16:53:45 -0300

    commit inicial do site

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Script para animação de scroll suave
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
